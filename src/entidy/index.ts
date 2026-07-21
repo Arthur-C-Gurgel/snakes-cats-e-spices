ce6e301ea4ae964bc7d9fa9bdffc82c0fe21684f
HEITOR JORGE MARQUES SANTOS <a95720527@rn.senac.local>
2026-07-21 12:14:37 -0300

    my firts commit

---
+++ src/entidy/index.ts
@@ -0,0 +1,9 @@
+import Prato from "./prato";
+
+const p = new Prato("Punheta de Bacalhau", 70, "Feita com lascas cruas do nobre bacalhau Gadus Morhua dessalgado, é combinada com azeite extravirgem premium, cebola roxa fatiada finamente e ervas frescas, resultando em uma entrada elegante, leve e vibrant")
+
+const pw = new Prato("Porra recheada", 15, "Consiste em uma massa frita em formato cilíndrico, estriada, crocante por fora e macia por dentro, recheada com doces como chocolate, doce de leite ou creme e polvilhada com açúcar e canela")
+
+console.log(p)
+
+console.log(pw)
