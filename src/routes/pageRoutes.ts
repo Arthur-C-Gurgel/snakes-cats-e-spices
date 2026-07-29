9142c390bc351be6b1e5f09d40ed150eda390a5f
ARTHUR-C-Gurgel <arthurcapistranogurgel@gmail.com>
2026-07-29 13:52:16 -0300

    apparently fixed bug of server not running as it should aka 'cannot get' problem

--- src/routes/pageRoutes.ts
+++ src/routes/pageRoutes.ts
@@ -9,6 +9,13 @@ app.use(express.json());
 
 app.use("/", pageRoutes);
 app.use("/api", apiRoutes);
+app.get('/', (req, res) => {
+    res.send('Welcome to the homepage!');
+    });
+    
+    app.get('/messages', (req, res) => {
+    res.send('Hello');
+    });
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
