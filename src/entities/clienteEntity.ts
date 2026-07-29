0eecb1414bdac5420b55cbbfcb3eb22f0adc256a
ARTHUR-C-Gurgel <arthurcapistranogurgel@gmail.com>
2026-07-29 13:17:39 -0300

    clienteEntity criado

---
+++ src/entities/clienteEntity.ts
@@ -0,0 +1,21 @@
+class Cliente {
+    id: number;
+
+    nome: string;
+
+    telefone: string;
+
+    email: string;
+
+    constructor(id: number, nome: string, telefone: string, email: string) {
+        this.id = id;
+
+        this.nome = nome;
+
+        this.telefone = telefone;
+
+        this.email = email;
+    }
+}
+
+export default Cliente;
